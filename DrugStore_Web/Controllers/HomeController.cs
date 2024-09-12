cdfa3fa00c8876a6d2c859ab5ba0ad3a0dd946c7
ngocminh04 <crushzone610@gmail.com>
2024-09-12 16:50:18 +0700

    Add project files.

---
+++ DrugStore_Web/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DrugStore_Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Đây là trang giới thiệu về nhà thuốc.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Đây là trang liên hệ nhà thuốc.";
+
+            return View();
+        }
+    }
+}
